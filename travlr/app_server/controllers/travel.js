3e085dc65872bcc2bd984d60f923aa398cedfc7e
zacheryirvin <zachirvin45@gmail.com>
2025-09-19 19:51:36 -0500

    Module 3 complete baseline

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,7 +1,9 @@
 // Get Travel View
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
-  res.render('travel', {title: 'Travlr Getaways'});
+  res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
-module.exports = { travel }
+module.exports = { travel };
