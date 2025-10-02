5ccd56649b4ed978e6a498eb21971363d618b66b
zacheryirvin <zachirvin45@gmail.com>
2025-10-02 07:51:43 -0500

    module 5 baseline

---
+++ travlr/app_api/controllers/trips.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Trip = require("../models/travlr");
+const Model = mongoose.model("trips");
+
+const tripsList = async(req, res) => {
+  const q = await Model
+    .find({})
+    .exec();
+
+  if(!q) {
+    return res
+      .status(404)
+      .json(err);
+  } else {
+    return res
+      .status(200)
+      .json(q);
+  }
+};
+
+const tripsFindByCode = async(req, res) => {
+  const q = await Model
+    .find({'code' : req.params.tripCode})
+    .exec();
+
+  if(!q) {
+    return res
+      .status(404)
+      .json(err);
+  } else {
+    return res
+      .status(200)
+      .json(q);
+  }
+};
+
+module.exports = {
+  tripsList,
+  tripsFindByCode
+};
--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,9 +1,35 @@
 // Get Travel View
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+//var fs = require('fs');
+//var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
-const travel = (req, res) => {
-  res.render('travel', {title: 'Travlr Getaways', trips});
+const tripsEndpoint = "http://localhost:3000/api/trips"
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
 };
 
-module.exports = { travel };
+const travel = async (req, res, next) => {
+  await fetch(tripsEndpoint, options)
+    .then((res) => res.json())
+    .then((json) => {
+      let message = null;
+      if(!(json instanceof Array)) {
+        message = "API Lookup Error";
+        json = [];
+      } else {
+        if(!json.length) {
+          message = "No Trips Exist In Our Database";
+        }
+      }
+      res.render("travel", {title: "Travlr Gateways", trips: json, message});
+    })
+    .catch((err) => res.status(500).send(err.message));
+};
+
+//const travel = (req, res) => {
+//  res.render("travel", {title: "Travlr Gateways", trips});
+//}
+
+module.exports = { travel, };
